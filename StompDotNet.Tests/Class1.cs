9c80aa782396e825a2668e2f70fa0c4f3fa9a8d1
Jerome Haltom <jhaltom@alethic.solutions>
2021-05-04 16:43:03 -0500

    remove test

--- StompDotNet.Tests/Class1.cs
+++ StompDotNet.Tests/Class1.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Net;
-using System.Threading;
-using System.Threading.Tasks;
-
-using Microsoft.AspNetCore.Connections;
-using Microsoft.Extensions.Logging.Abstractions;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace StompDotNet.Tests
 {
@@ -13,16 +6,6 @@ namespace StompDotNet.Tests
     [TestClass]
     public class Class1
     {
-
-        [TestMethod]
-        public async Task Foo()
-        {
-            var cancellationToken = CancellationToken.None;
-            var f = new StompWebSocketConnectionFactory(c => { c.Credentials = new NetworkCredential("admin", "admin"); }, NullLogger.Instance);
-            var e = new UriEndPoint(new Uri("ws://localhost:44112/websockets/messaging/websocket"));
-            var c = await f.OpenAsync(e, cancellationToken);
-            await Task.Delay(TimeSpan.FromMinutes(1));
-        }
 
     }
 
